7ea700c53c8804586807e171841bc4abe91ab173
SpaceX12 <58943699+SpaceX12@users.noreply.github.com>
2022-07-10 16:03:39 +0530

    Add files via upload

---
+++ Scrape.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import csv
+
+URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+r = requests.get(URL)
+
+def scrape():
+    titles = ["Name", "Distance", "Mass", "Radius"]
+    stars = []
+
+    r = requests.get(url = URL)
+    
+    for i in range(1, 97):
+        soup = BeautifulSoup(r.content, "html.parser")
+        ults = soup.find_all("tr")[1]
+
+        for ult in ults :
+            td_tags = soup.find_all("td")
+            temp = []
+
+            for index, td_tag in enumerate(td_tags):
+                if index == 1:
+                    temp.append(td_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp.append(td_tag.contents[0])
+                    except:
+                        temp.append("")
+                        
+                stars.append(temp)
+    
+    with open("StarResearch.csv", "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(titles)
+        writer.writerows(stars)
+
+scrape()
+
